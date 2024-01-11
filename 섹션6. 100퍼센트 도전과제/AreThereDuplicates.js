53f5bb5dcf55e37fc41a5a44c68efdc55f6703d3
심호림 <simhorim@simhorim-ui-MacBookPro.local>
2024-01-11 10:58:11 +0900

    섹션6 AreThereDuplicates 문제풀이

---
+++ 섹션6. 100퍼센트 도전과제/AreThereDuplicates.js
@@ -0,0 +1,87 @@
+// 문제
+// 가변적인 수의 인수(a variable number of arguments)를 받아들이고 전달된 인자 중 중복이 있는지 확인하는 areThereDuplicates라는 함수를 구현합니다.
+// 빈도 카운터 패턴 또는 다중 포인터 패턴을 사용하여 이 문제를 해결할 수 있습니다.
+
+// ## 빈도수 세기
+// 나머지 변수를 활용하여 입력받은 값들을 배열화 한다.
+// 빈 객체를 생성한다
+// 빈도수 세기를 활용하여 해당 배열의 각 값의 빈도수를 객체에 넣는다.
+// 만약 key에 대한 value가 1보다 크다면 true를 반환한다
+// 그게 아니라면 false를 반환한다.
+
+// function areThereDuplicates(...val) {
+//     let frequency = {};
+
+//     for(let i = 0; i < val.length; i ++) {
+//         frequency[val[i]] = (frequency[val[i]] || 0) + 1;
+//     }
+
+//     for(let key in frequency) {
+//         if(frequency[key] > 1) {
+//             return true;
+//         }
+//     }
+//     return false;
+//   }
+
+// areThereDuplicates 솔루션 (빈도 수 세기)
+// function areThereDuplicates(...arguments) {
+//     let collection = {}
+//     for(let val in arguments){
+//       collection[arguments[val]] = (collection[arguments[val]] || 0) + 1
+//     }
+//     for(let key in collection){
+//       if(collection[key] > 1) return true
+//     }
+//     return false;
+//   }
+
+// feedback
+// 이 것은 코드가 동일 
+// 차이점은 for i 대신 for in을 썼다는 정도? 배열의 반복문의 경우 for of나 for in을 쓰는게 효율적일듯 
+
+
+// ## 다중 포인터
+// 나머지 변수로 배열화한다.
+// 배열을 크기대로 분류한다.
+// index 0 값을 left, index 1 값을 right로 설정하여 index를 1씩 늘려가며 중복인지 확인한다
+// 중복이 있는 경우 false를 리턴한다.
+// 중복이 없는 경우 true를 리턴한다.
+
+function areThereDuplicates(...val) {
+  const ARR = val.sort();
+
+  console.log(ARR);
+  for (let i = 0; i < ARR.length; i++) {
+    let left = i;
+    let right = i + 1;
+    if (ARR[left] === ARR[right]) {
+      return true;
+    }
+  }
+  return false;
+}
+
+// areThereDuplicates 솔루션 (다중 포인터)
+// function areThereDuplicates(...args) {
+//   // Two pointers
+//   args.sort((a,b) => a > b);
+//   let start = 0;
+//   let next = 1;
+//   while(next < args.length){
+//     if(args[start] === args[next]){
+//         return true
+//     }
+//     start++
+//     next++
+//   }
+//   return false
+// }
+
+// areThereDuplicates One Liner 솔루션
+// function areThereDuplicates() {
+//   return new Set(arguments).size !== arguments.length;
+// }
+
+// feedback
+// Set.size 이용하는 거 그렇게 많이 다뤘는데 생각하지 못했다는게 아쉽.. 해당 방법에 대해서 생각하는 것도 좋지만 기존에 알던 지식을 떠올려보기
--- 섹션6. 100퍼센트 도전과제/sameFrequency.js
+++ 섹션6. 100퍼센트 도전과제/sameFrequency.js
@@ -1,5 +1,7 @@
 // 두 개의 양의 정수가 주어졌을 때, 두 숫자의 자릿수가 같은 빈도를 갖는지 구합니다.
-// 입력받은 값을 각각 배열화 한다.
+
+// ## 빈도수 세기
+// 숫자를 문자로 바꾸어 배열화 한 이후 그 배열의 요소들을 숫자화 한다.
 // 빈 객체를 만든다.
 // 각 배열을 각 객체에 키 값과 빈도수를 넣는다.
 // 각 객체의 key 값은 일치하는지 value 값은 일치하는지 비교한다.
@@ -7,23 +9,21 @@
 function sameFrequency(num1, num2) {
   const STRING_NUM1 = String(num1);
   const STRING_NUM2 = String(num2);
-  
+
   const mapfn = (arg) => Number(arg);
 
   const ARR1 = Array.from(STRING_NUM1, mapfn);
   const ARR2 = Array.from(STRING_NUM2, mapfn);
-  
+
   let num1Frequency = {};
   let num2Frequency = {};
 
   for (let num of ARR1) {
     num1Frequency[num] = (num1Frequency[num] || 0) + 1;
   }
-
   for (let num of ARR2) {
     num2Frequency[num] = (num2Frequency[num] || 0) + 1;
   }
-
   for (let key in num1Frequency) {
     if (!(key in num2Frequency)) {
       return false;
@@ -36,3 +36,31 @@ function sameFrequency(num1, num2) {
 }
 
 console.log(sameFrequency(123, 321));
+
+// sameFrequency 솔루션
+// function sameFrequency(num1, num2){
+//   let strNum1 = num1.toString();
+//   let strNum2 = num2.toString();
+//   if(strNum1.length !== strNum2.length) return false;
+  
+//   let countNum1 = {};
+//   let countNum2 = {};
+  
+//   for(let i = 0; i < strNum1.length; i++){
+//     countNum1[strNum1[i]] = (countNum1[strNum1[i]] || 0) + 1
+//   }
+  
+//   for(let j = 0; j < strNum1.length; j++){
+//     countNum2[strNum2[j]] = (countNum2[strNum2[j]] || 0) + 1
+//   }
+  
+//   for(let key in countNum1){
+//     if(countNum1[key] !== countNum2[key]) return false;
+//   }
+ 
+//   return true;
+// }
+
+// ## feedback
+// Stirng 외에 문자열로 바꾸는 방법 중 toString() 또한 있다는 사실
+// 나의 경우 key 값은 존재하는지 또 key 값에 대한 value 값이 존재하는지 둘다 고려하였는데 key 값에 대한 value 값만 고려해도 둘다 포괄할 수 있다. 다만 이러기 위해서는 두 문자열의 길이가 동일하다는 전제가 있어야함.
