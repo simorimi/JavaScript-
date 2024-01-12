ebe2622b6f88c604ced1941bff54c1d4a5709259
simhorim <hori5020@gmail.com>
2024-01-12 17:14:07 +0900

    isSubsequence 문제풀이 및 솔루션

--- 섹션6. 100퍼센트 도전과제/AveragePair.js
+++ 섹션6. 100퍼센트 도전과제/AveragePair.js
@@ -3,7 +3,7 @@
 // 정렬된 정수 배열과 목표 평균이 주어졌을 때, 배열에 쌍의 평균이 목표 평균과 같은 값의 쌍이 있는지 확인합니다.
 // 목표 평균과 일치하는 쌍이 두 개 이상 있을 수 있습니다.
 
-// 다중 포인터
+// my solution
 // 배열과 숫자를 입력받는다.
 // index 0의 left 변수와 index 끝의 right 변수를 생성한다.
 // 배열[left] 값과 배열[right] 값의 평균이 주어진 평균보다 큰지 작은지 확인한다.
@@ -33,3 +33,20 @@ function averagePair(arr, avg) {
 }
 
 console.log(averagePair([1,2,3,4,5], 6));
+
+// averagePair 솔루션
+// function averagePair(arr, num){
+//   let start = 0
+//   let end = arr.length-1;
+//   while(start < end){
+//     let avg = (arr[start]+arr[end]) / 2 
+//     if(avg === num) return true;
+//     else if(avg < num) start++
+//     else end--
+//   }
+//   return false;
+// }
+
+// feedback
+// 평균값에 대한 반복 부분을 변수로 설정하여 썼으면 훨씬 깔끔한 코드를 작성할 수 있었을듯
+// 반복에 대해서는 줄이려고 하자
--- 섹션6. 100퍼센트 도전과제/isSubsequence.js
+++ 섹션6. 100퍼센트 도전과제/isSubsequence.js
@@ -1,6 +1,73 @@
 // 다중 포인터 - isSubsequence
-// 두 문자열을 받아 첫 번째 문자열의 문자가 두 번째 문자열의 문자의 일부에 포함되는지 확인하는 isSubsequence라는 함수를 작성합니다. 
+// 두 문자열을 받아 첫 번째 문자열의 문자가 두 번째 문자열의 문자의 일부에 포함되는지 확인하는 isSubsequence라는 함수를 작성합니다.
 // 즉, 이 함수는 첫 번째 문자열의 문자가 순서가 바뀌지 않고 두 번째 문자열의 어딘가에 나타나는지 확인해야 합니다.
 
-// 다중 포인터
-//
+// my solution
+// 두 문자열을 입력 받는다.
+// index 0값을 활용하여 left, right 변수를 생성한다.
+// str2[right]값이 str1[left]의 값이랑 동일한지 확인한다. 동일하면 left값을 +1 한다.
+// 다음 동일한 문자가 있는지 파악하기 위하여 right 값에 ++를 한다. 
+// 해당 과정을 반복하여 left 값이 str1 길이와 같다면 ture를 반환한다.
+// 아니라면 false를 반환한다.
+
+function isSubsequence(str1, str2) {
+    let left = 0;
+    let right = 0;
+    let count = 0;
+    while(right < str2.length) {
+        if(str2[right] === str1[count]) {
+            count ++;
+            left = right;
+        }
+        if(count === str1.length) {
+            return true;
+        }
+        right ++;
+    }
+    return false;
+}
+
+// refactoring
+function isSubsequence(str1, str2) {
+    let right = 0;
+    let left = 0;
+    while(right < str2.length) {
+        if(str2[right] === str1[left]) {
+            left ++;
+        }
+        if(left === str1.length) {
+            return true;
+        }
+        right ++;
+    }
+    return false;
+}
+
+
+// isSubsequence 솔루션 - 반복
+// function isSubsequence(str1, str2) {
+//   var i = 0;
+//   var j = 0;
+//   if (!str1) return true;
+//   while (j < str2.length) {
+//     if (str2[j] === str1[i]) i++;
+//     if (i === str1.length) return true;
+//     j++;
+//   }
+//   return false;
+// }
+
+// isSubsequence 솔루션 – O(1) 공간이 아닌 재귀
+// function isSubsequence(str1, str2) {
+//   if(str1.length === 0) return true
+//   if(str2.length === 0) return false
+//   if(str2[0] === str1[0]) return isSubsequence(str1.slice(1), str2.slice(1))  
+//   return isSubsequence(str1, str2.slice(1))
+// }
+
+// 재귀는 재귀를 공부하고 난 다음에 보도록 하고
+// 나는 다중 포인터에 대해서 한쪽 문자열에서만 생각하였는데 그게 아니라 각 문자열의 하나씩의 포인터를 가지고도 할 수 있겠다는 생각이 들었다.
+// 변수의 경우 적을수록 좋으니 
+// 심지어 나의 경우도 left를 전혀 쓰지 않는다.
+// 코드를 다 작성한 이후 다시 한번 보고 refactoring 과정을 거치도록 
+
